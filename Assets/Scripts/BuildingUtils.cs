21de49033334384b45de5f5a867da0f3f96fd77e
elliotmiller18 <egmiller@umich.edu>
2025-11-05 16:26:33 -0500

    added grid

---
+++ Assets/Scripts/BuildingUtils.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+
+public enum BuildingType
+{
+    TreeOfLife,
+    BananaFarm,
+    ArcherTower,
+}
+
+public struct BuildingDimensions
+{
+    public readonly int width;
+    public readonly int height;
+
+    public BuildingDimensions(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+}
+
+public class BuildingUtils
+{
+    private static Dictionary<BuildingType, BuildingDimensions> typeToDimensions = new()
+    {
+        { BuildingType.TreeOfLife, new BuildingDimensions(4, 4) },
+        { BuildingType.BananaFarm, new BuildingDimensions(1, 1) },
+        { BuildingType.ArcherTower, new BuildingDimensions(1, 1) }
+    };
+
+    public static BuildingDimensions TypeToDimensions(BuildingType type)
+    {
+        return typeToDimensions[type];
+    }
+
+    //TODO: building to prefab
+}
+
+public class Building
+{
+    public readonly BuildingDimensions dimensions;
+    public readonly BuildingType type;
+
+    public Building(BuildingType in_type)
+    {
+        type = in_type;
+        dimensions = BuildingUtils.TypeToDimensions(type);
+    }
+}
---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Grid : MonoBehaviour
+{
+    public static Grid instance;
+
+    public const int GRID_SIZE = 200;
+    private Building[,] grid = new Building[GRID_SIZE, GRID_SIZE];
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Debug.LogError("duplicate grid on " + gameObject.name + " destroying.");
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+    }
+
+    public bool CanPlace(int x_start, int y_start, Building building)
+    {
+        int x_end = building.dimensions.width + x_start;
+        int y_end = building.dimensions.height + y_start;
+
+        Assert.IsFalse(x_end > GRID_SIZE, "placing building out of bounds");
+        Assert.IsTrue(x_start >= 0, "placing building out of bounds");
+        Assert.IsFalse(y_end > GRID_SIZE, "placing building out of bounds");
+        Assert.IsTrue(y_start >= 0, "placing building out of bounds");
+
+        if (x_start < 0 || y_start < 0 || x_end > GRID_SIZE || y_end > GRID_SIZE) return false;
+
+        for (int x = x_start; x < x_end; x++)
+        {
+            for (int y = y_start; y < y_end; y++)
+            {
+                if (grid[x, y] is not null) return false;
+            }
+        }
+        return true;
+    }
+
+    public void Place(int x_start, int y_start, Building building)
+    {
+        Assert.IsTrue(CanPlace(x_start, y_start, building), "Trying to place building that can't be placed!");
+
+        int x_end = building.dimensions.width + x_start;
+        int y_end = building.dimensions.height + y_start;
+
+        for (int x = x_start; x < x_end; x++)
+        {
+            for (int y = y_start; y < y_end; y++)
+            {
+                grid[x, y] = building;
+            }
+        }
+    }
+}
+
