86cbd80c470e46d91144d0d8314f69da4f512f32
srhklm <157084753+srhklm@users.noreply.github.com>
2025-11-06 18:14:09 -0500

    fixed ui and added toasts

--- Assets/Scripts/MenuPanel.cs
+++ Assets/Scripts/MenuPanel.cs
@@ -5,6 +5,7 @@ public class MenuPanel : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private GameObject menuPanel;
+    private bool isVisible = false;
     void Start()
     {
         menuPanel = this.gameObject;
@@ -20,5 +21,11 @@ public class MenuPanel : MonoBehaviour
     public void ShowMenu()
     {
         menuPanel.SetActive(true);
+    }
+
+    public void ToggleMenu()
+    {
+        isVisible = !isVisible;
+        menuPanel.SetActive(isVisible);
     }
 }
---
+++ Assets/Scripts/ToastManager.cs
@@ -0,0 +1,201 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.Rendering;
+using Unity.VisualScripting;
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine.WSA;
+
+public struct ToastData
+{
+    public string message;
+    public float duration;
+    public float fadeDuration;
+    public bool waitForInput;
+    public ToastData(string message, float duration = 2.0f, float fadeDuration = 0.5f, bool waitForInput = false)
+    {
+        this.message = message;
+        this.duration = duration;
+        this.fadeDuration = fadeDuration;
+        this.waitForInput = waitForInput;
+    }
+}
+
+public class ToastManager : MonoBehaviour
+{
+    public static ToastManager Instance;
+    [SerializeField] private TextMeshProUGUI toastText;
+    [SerializeField] private GameObject toastPanel;
+    [SerializeField] private CanvasGroup toastCanvasGroup;
+    [SerializeField] private AudioSource toastAudioSource;
+    [SerializeField] private AudioClip toastSound;
+    [SerializeField] private GameObject continuePrompt;
+
+    Queue<ToastData> toastQueue = new Queue<ToastData>();
+
+    private bool isShowingToast = false;
+    private bool waitingForInput = false;
+
+    public bool freezeToasts = false;
+
+    void Start()
+    {
+        toastPanel.SetActive(false);
+        continuePrompt.SetActive(false);
+    }
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            RequestToast("This is a test toast message!", duration: 1.0f, fadeDuration: 0.3f);
+        }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            RequestToast("This is another toast message!", duration: 1.0f, fadeDuration: 0.3f, waitForInput: true);
+            RequestToast("Hello", duration: 1.0f, fadeDuration: 0.3f, waitForInput: true);
+            RequestToast("I'm", duration: 1.0f, fadeDuration: 0.3f, waitForInput: true);
+            RequestToast("Sarah", duration: 1.0f, fadeDuration: 0.3f, waitForInput: true);
+            RequestToast("6", duration: 1.0f, fadeDuration: 0.3f, waitForInput: true);
+            RequestToast("7", duration: 1.0f, fadeDuration: 0.3f, waitForInput: true);
+        }
+        if (waitingForInput && Input.GetKeyDown(KeyCode.Space))
+        {
+            SkipToNextToast();
+        }
+        if (!freezeToasts)
+        {
+            ProcessToastQueue();
+        }
+    }
+
+    public void RequestToast(string message, float duration = 2.0f, float fadeDuration = 0.5f, bool waitForInput = false)
+    {
+        ToastData toastData = new ToastData(message, duration, fadeDuration, waitForInput);
+        toastQueue.Enqueue(toastData);
+    }
+
+    public void SkipToNextToast()
+    {
+        if (waitingForInput)
+        {
+            waitingForInput = false;
+            StopAllCoroutines();
+            StartCoroutine(FadeOutAndNext());
+        }
+    }
+    public void ForceEndCurrentToast()
+    {
+        if (isShowingToast)
+        {
+            StopAllCoroutines();
+            toastCanvasGroup.alpha = 0f;
+            toastPanel.SetActive(false);
+            if (continuePrompt != null)
+                continuePrompt.SetActive(false);
+            isShowingToast = false;
+            waitingForInput = false;
+        }
+    }
+
+    private void ProcessToastQueue()
+    {
+        if (!isShowingToast && toastQueue.Count > 0)
+        {
+            ToastData nextToast = toastQueue.Dequeue();
+            StartCoroutine(DoToast(nextToast));
+        }
+    }
+
+    IEnumerator DoToast(ToastData data)
+    {
+        if (isShowingToast)
+        {
+            yield break;
+        }
+        isShowingToast = true;
+        toastText.text = data.message;
+        toastPanel.SetActive(true);
+
+        float initialTime = Time.time;
+        float progress = 0.0f;
+
+        while (progress < 1.0f)
+        {
+            progress = (Time.time - initialTime) / data.fadeDuration;
+            toastCanvasGroup.alpha = Mathf.Lerp(0, 1, progress);
+            yield return null;
+        }
+        toastCanvasGroup.alpha = 1.0f;
+
+        if (data.waitForInput)
+        {
+            if (continuePrompt != null)
+                continuePrompt.SetActive(true);
+                
+            waitingForInput = true;
+            
+            // Wait until spacebar is pressed (handled in Update)
+            while (waitingForInput)
+            {
+                yield return null;
+            }
+            
+            if (continuePrompt != null)
+                continuePrompt.SetActive(false);
+        }
+        else
+        {
+            yield return new WaitForSeconds(data.duration);
+        }
+
+        initialTime = Time.time;
+        progress = 0.0f;
+
+        while (progress < 1.0f)
+        {
+            progress = (Time.time - initialTime) / data.fadeDuration;
+            toastCanvasGroup.alpha = Mathf.Lerp(1, 0, progress);
+            yield return null;
+        }
+
+        toastCanvasGroup.alpha = 0.0f;
+        toastPanel.SetActive(false);
+        isShowingToast = false;
+    }
+    IEnumerator FadeOutAndNext()
+    {
+        if (continuePrompt != null)
+            continuePrompt.SetActive(false);
+            
+        float initialTime = Time.time;
+        float progress = 0.0f;
+        float quickFade = 0.2f;
+
+        while (progress < 1.0f)
+        {
+            progress = (Time.time - initialTime) / quickFade;
+            toastCanvasGroup.alpha = Mathf.Lerp(toastCanvasGroup.alpha, 0, progress);
+            yield return null;
+        }
+
+        toastCanvasGroup.alpha = 0.0f;
+        toastPanel.SetActive(false);
+        isShowingToast = false;
+    }
+}
