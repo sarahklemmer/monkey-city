a67b378ba16c854c3f0ccc51863f47a951168131
elliotmiller18 <egmiller@umich.edu>
2025-11-23 21:19:09 -0500

    made arrow thicker saved scene and fixed bug where wed unlock spiketrap over and over and over

--- Assets/Scripts/GameLogic/BuildingProgression.cs
+++ Assets/Scripts/GameLogic/BuildingProgression.cs
@@ -5,7 +5,11 @@ public class BuildingProgression : MonoBehaviour
     void Update()
     {   
         // unlock traps if we've placed a bananafarm and archertower
-        if(BuildingManager.instance.GetBuildingsOfType(BuildingType.BananaFarm).Count > 0 && BuildingManager.instance.GetBuildingsOfType(BuildingType.ArcherTower).Count > 0)
+        if(
+            BuildingManager.instance.GetBuildingsOfType(BuildingType.BananaFarm).Count > 0 && 
+            BuildingManager.instance.GetBuildingsOfType(BuildingType.ArcherTower).Count > 0 &&
+            !BuildingUnlock.Unlocked(BuildingType.SpikeTrap)
+            )
         {
             BuildingUnlock.Unlock(BuildingType.SpikeTrap);
         }
